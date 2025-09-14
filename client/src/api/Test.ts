06dc04aa35d16e9afc5b3f7921208a846243eae9
SarojKumarRanjan <sarojranjan555@gmail.com>
2025-09-14 10:01:37 +0530

    changed backend url

--- client/src/api/Test.ts
+++ client/src/api/Test.ts
@@ -1,7 +1,7 @@
 import { generatePayload } from "@/types";
 import axios from "axios";
 
-const API_BASE_URL =  "https://mcqbackend-9bxv.onrender.com";
+const API_BASE_URL =  "https://mcqbackend-22in.onrender.com";
 
 
 export const generateTest = async (payload: generatePayload) => {
